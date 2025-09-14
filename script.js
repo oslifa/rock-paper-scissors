bb9d06987785a8e409c765088f2b0dd6242f2483
oslifa <224970287+oslifa@users.noreply.github.com>
2025-09-14 22:01:10 +0100

    Add function to randomly choose and return "rock", "paper" or "scissors" for player opponent

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("test");
+function getComputerChoice() {
+    switch(Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock";
+
+        case 1:
+            return "paper";
+
+        case 2:
+            return "scissors";
+    }
+}
