9b8fbc57e7419195938f4a0b3d82513e93b8fdd5
oslifa <224970287+oslifa@users.noreply.github.com>
2025-09-28 00:28:46 +0100

    Modify function to play round in order to display results and reset game after 5 points reached

--- script.js
+++ script.js
@@ -11,56 +11,11 @@ function getComputerChoice() {
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    // function playRound(humanChoice) {
-    //     let computerChoice = getComputerChoice();
-    //     let roundMessage = `${humanChoice.toUpperCase()} VS. ${computerChoice.toUpperCase()}? `;
-    //     let win;
-
-    //     if (humanChoice == computerChoice) {
-    //         roundMessage += "IT'S A TIE!"
-    //     } else {
-    //         switch (humanChoice) {
-    //             case "rock":
-    //                 win = "paper";
-    //                 break;
-    //             case "paper":
-    //                 win = "scissors";
-    //                 break;
-    //             case "scissors":
-    //                 win = "rock";
-    //                 break;
-    //         }
-
-    //         if (computerChoice == win) {
-    //             roundMessage += "YOU LOSE!";
-    //             computerScore++;
-    //         } else {
-    //             roundMessage += "YOU WIN!";
-    //             humanScore++;
-    //         }
-    //     }
-
-    //     console.log(roundMessage);
-    // }
-
-    // for (let i = 0; i < 5; i++) {
-    //     playRound();
-    // }
-
-    let endMessage = `PLAYER: ${humanScore} PTS. CPU: ${computerScore} PTS.`;
-
-    if (humanScore == computerScore) {
-        console.log(endMessage += ` IT'S A TIE!`);
-    } else if (humanScore > computerScore) {
-        console.log(endMessage += ` YOU WIN!`);
-    } else {
-        console.log(endMessage += ` YOU LOSE!`);
-    }
-}
+const roundDisplay = document.querySelector("#roundDisplay");
+const scoreDisplay = document.querySelector("#scoreDisplay");
 
 function playRound(humanChoice) {
     let computerChoice = getComputerChoice();
@@ -84,14 +39,32 @@ function playRound(humanChoice) {
 
         if (computerChoice == win) {
             roundMessage += "YOU LOSE!";
+            computerScore++;
         } else {
             roundMessage += "YOU WIN!";
+            humanScore++;
         }
     }
 
-    console.log(roundMessage);
-}
+    roundDisplay.textContent = `${roundMessage}`;
 
+    let scoreMessage = `PLAYER: ${humanScore} PTS. CPU: ${computerScore} PTS.`;
+
+    if (humanScore == 5 || computerScore == 5) {
+        if (humanScore == computerScore) {
+            scoreMessage += ` IT'S A TIE!`;
+        } else if (humanScore > computerScore) {
+            scoreMessage += ` GAME OVER! YOU WIN!`;
+        } else {
+            scoreMessage += ` GAME OVER! YOU LOSE!`;
+        }
+
+        humanScore = 0;
+        computerScore = 0;
+    }
+
+    scoreDisplay.textContent = `${scoreMessage}`;
+}
 
 document.body.addEventListener("click", (event) => {
     if (event.target.classList.contains("choice")) {
