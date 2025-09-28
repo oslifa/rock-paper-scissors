74fb0d60dec474b6cebb9d00bc0cd4e7bcad0b67
oslifa <224970287+oslifa@users.noreply.github.com>
2025-09-29 00:32:30 +0100

    Modify function to clear previous game results then list results for all rounds of a new 5-point game

--- script.js
+++ script.js
@@ -13,17 +13,33 @@ function getComputerChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
+let isTieRound = false;
+let isGameOver = false;
 
-const roundDisplay = document.querySelector("#roundDisplay");
-const scoreDisplay = document.querySelector("#scoreDisplay");
+let results = document.querySelector("#results");
 
 function playRound(humanChoice) {
+    if (isTieRound) {
+        let tieContainer = document.querySelector(`#round${round}`);
+        results.removeChild(tieContainer);
+    } else {
+        round++;
+    }
+
+    if (isGameOver) {
+        isGameOver = false;
+        results.innerHTML = "";
+    }
+    
     let computerChoice = getComputerChoice();
-    let roundMessage = `${humanChoice.toUpperCase()} VS. ${computerChoice.toUpperCase()}: `;
+    let roundMessage = `ROUND ${round}: ${humanChoice.toUpperCase()} VS. ${computerChoice.toUpperCase()}! `;
     let win;
+    isTieRound = false;
 
     if (humanChoice == computerChoice) {
-        roundMessage += "IT'S A TIE!"
+        roundMessage += "IT'S A TIE! REDO!"
+        isTieRound = true;
     } else {
         switch (humanChoice) {
             case "rock":
@@ -46,24 +62,41 @@ function playRound(humanChoice) {
         }
     }
 
+    const roundDisplay = document.createElement("p");
     roundDisplay.textContent = `${roundMessage}`;
 
     let scoreMessage = `PLAYER: ${humanScore} PTS. CPU: ${computerScore} PTS.`;
+    const scoreDisplay = document.createElement("p");
+    scoreDisplay.textContent = `${scoreMessage}`;
+
+    let gameMessage;
 
     if (humanScore == 5 || computerScore == 5) {
         if (humanScore == computerScore) {
-            scoreMessage += ` IT'S A TIE!`;
+            gameMessage = ` IT'S A TIE!`;
         } else if (humanScore > computerScore) {
-            scoreMessage += ` GAME OVER! YOU WIN!`;
+            gameMessage = ` GAME OVER! YOU WIN!`;
         } else {
-            scoreMessage += ` GAME OVER! YOU LOSE!`;
+            gameMessage = ` GAME OVER! YOU LOSE!`;
         }
 
+        round = 0;
         humanScore = 0;
         computerScore = 0;
+        isGameOver = true;
     }
 
-    scoreDisplay.textContent = `${scoreMessage}`;
+    const gameDisplay = document.createElement("p");
+    gameDisplay.textContent = gameMessage;
+
+    let container = document.createElement("div");
+    container.id = `round${round}`;
+    
+    container.appendChild(roundDisplay);
+    container.appendChild(scoreDisplay);
+    container.appendChild(gameDisplay);
+    
+    results.appendChild(container);
 }
 
 document.body.addEventListener("click", (event) => {
